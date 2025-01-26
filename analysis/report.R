0cd77db4708078c49101ae4ea27c34ba953d09f5
C Andrew Basham, PhD <151640044+CAndrewBasham@users.noreply.github.com>
2025-01-26 06:33:36 +0000

    Generate age histograms

--- analysis/report.R
+++ analysis/report.R
@@ -6,7 +6,7 @@ df_input <- read_csv(
 )
 
 plot_age <- ggplot(data=df_input, aes(df_input$age)) + geom_histogram()
-
+ 
 ggsave(
   plot= plot_age,
   filename="report.png", path=here::here("output"),
